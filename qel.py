e40766021885bd1f55143213234eeef8489228b7
debrevitatevitae <giotb92@gmail.com>
2023-11-07 17:07:16 +0100

    Complete 1: data gen and split

--- qel.py
+++ qel.py
@@ -9,7 +9,7 @@ Steps
 1. Generate random uniformly distributed samples and labels
     1a. Split into a train and a test set
 2. Build the QNN
-    2a. Apply RY(2jarccos(x)) feature map. Params: x.
+    2a. Apply RY(2jarccos(x)) feature map. Params: x.s
     2b. Apply Hardware Efficient Ansatz (HEA). Params: \theta.
     2c. Calculate the expectation value <M>=<Z^{\otimes n}>.
 3. Define loss function (MSE).
@@ -21,9 +21,24 @@ Steps
     6b. Check maximisation trajectory
 """
 import pennylane as qml
+from pennylane import numpy as np
+
+
+def train_test_split(X, y, n_test=1):
+    test_idxs = np.random.choice(len(y), size=n_test, replace=False)
+    train_idxs = np.setdiff1d(np.arange(len(y)), test_idxs)
+    return X[train_idxs], y[train_idxs], X[test_idxs], y[test_idxs]
+
 
 def main():
-    pass
+    np.random.seed = 42
+
+    M = 30
+    X = np.random.uniform(low=0., high=1., size=M).reshape(-1, 1)
+    y = np.sin(5*X).reshape(X.shape[0],)
+
+    X_train, y_train, X_test, y_test = train_test_split(X, y, n_test=10)
+
 
 if __name__ == '__main__':
     main()
